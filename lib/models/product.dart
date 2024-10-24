6cbd43bcdcda97a05b20a3e87d351cc23bcdbaa6
zekeriya2280 <zekeriyacoskun2280@gmail.com>
2024-10-24 11:34:18 +0900

    added product models style to Firebase factory model not finished

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,8 +3,8 @@ class Product {
   double basePurchasePrice; // Ürünün temel alış fiyatı
   double baseSalePrice; // Ürünün temel satış fiyatı
   final int duration; // Ürünün üretim süresi
-  final Map<String, int>? requiredMaterials; // Ürünün üretimi için gereken hammaddeler
-  double inflationRate; // Ürünün enflasyon oranı
+  final List<Map<String, int>>? requiredMaterials; // Ürünün üretimi için gereken hammaddeler
+  double inflation; // Ürünün enflasyon oranı
 
   Product({
     required this.name,
@@ -12,12 +12,12 @@ class Product {
     required this.baseSalePrice,
     required this.duration,
     this.requiredMaterials,
-    this.inflationRate = 0.02, // Varsayılan enflasyon oranı %2
+    this.inflation = 0.02, // Varsayılan enflasyon oranı %2
   });
 
   // Enflasyona göre alış fiyatını güncelle
-  double get currentPurchasePrice => basePurchasePrice * (1 + inflationRate);
+  double get currentPurchasePrice => basePurchasePrice * (1 + inflation);
 
   // Enflasyona göre satış fiyatını güncelle
-  double get currentSalePrice => baseSalePrice * (1 + inflationRate);
+  double get currentSalePrice => baseSalePrice * (1 + inflation);
 }
